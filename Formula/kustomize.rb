9cfe262dac3b7606ff07e383bd572adf7c5822b1
Alex Thompson <alex.thompson@semgrep.com>
2023-04-24 10:47:12 -0400

    Add `kustomize` Formula (#4)

---
+++ Formula/kustomize.rb
@@ -0,0 +1,16 @@
+class Kustomize < Formula
+  desc "Template-free customization of Kubernetes YAML manifests"
+  homepage "https://github.com/kubernetes-sigs/kustomize"
+  on_arm do
+    url "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.2.0/kustomize_v4.2.0_darwin_arm64.tar.gz"
+    sha256 "7ad70475fe5684f7150f7f1825df5f17652ec812fa65129b756000e9a6b49fff"
+  end
+  on_intel do
+    url "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.2.0/kustomize_v4.2.0_darwin_amd64.tar.gz"
+    sha256 "808d86fc15cec9226dd8b6440f39cfa8e8e31452efc70fb2f35c59529ddebfbf"
+  end
+
+  def install
+    bin.install "kustomize"
+  end
+end
