387b00ca115af960e76e212be598db6698bd8660
Ben Fulton <ben.fulton@semgrep.com>
2023-11-13 13:40:00 -0500

    Update kustomize

--- Formula/kustomize.rb
+++ Formula/kustomize.rb
@@ -3,11 +3,11 @@ class Kustomize < Formula
   homepage "https://github.com/kubernetes-sigs/kustomize"
   version "4.2.0"
   on_arm do
-    url "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.2.0/kustomize_v4.2.0_darwin_arm64.tar.gz"
+    url "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.5.7/kustomize_v4.5.7_darwin_arm64.tar.gz"
     sha256 "7ad70475fe5684f7150f7f1825df5f17652ec812fa65129b756000e9a6b49fff"
   end
   on_intel do
-    url "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.2.0/kustomize_v4.2.0_darwin_amd64.tar.gz"
+    url "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.5.7/kustomize_v4.5.7_darwin_amd64.tar.gz"
     sha256 "808d86fc15cec9226dd8b6440f39cfa8e8e31452efc70fb2f35c59529ddebfbf"
   end
 
