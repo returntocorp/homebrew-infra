c43bf2fd6cc615208b776bd171e7b91a9ff710db
Ben Fulton <ben.fulton@semgrep.com>
2023-11-13 15:07:43 -0500

    chore: Update `kubectl` and `kustomize` versions for k8s 1.26 (#8)
    * Create .gitignore
    
    * Update kubernetes-cli
    
    * Update kustomize
    
    * Update SHAs

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -2,12 +2,12 @@ class KubernetesCli < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   on_arm do
-    url "https://dl.k8s.io/release/v1.24.13/bin/darwin/arm64/kubectl"
-    sha256 "a0b30642de4e0c290e08f1895d7a0951a004f23d107bef86a6460c5f5b516585"
+    url "https://dl.k8s.io/release/v1.26.10/bin/darwin/arm64/kubectl"
+    sha256 "e356b0262e2c3b2e653ea699149620361cf1381e98732bf173c8243089167605"
   end
   on_intel do
-    url "https://dl.k8s.io/release/v1.24.13/bin/darwin/amd64/kubectl"
-    sha256 "eb0dbc5e55c604b42b19d51609cb2e52e6da9bcde879cdb07d20d74da061be08"
+    url "https://dl.k8s.io/release/v1.26.10/bin/darwin/amd64/kubectl"
+    sha256 "325a65e3ba0ece81be327f68dfe9132c2c6befd209c0a6a4463cc9668add2e37"
   end
 
   def install
