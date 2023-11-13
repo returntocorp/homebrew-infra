a0e6ffe29850626692bfdf78196ab882f8cff07b
Ben Fulton <ben.fulton@semgrep.com>
2023-11-13 15:00:50 -0500

    Update SHAs

--- Formula/kustomize.rb
+++ Formula/kustomize.rb
@@ -4,11 +4,11 @@ class Kustomize < Formula
   version "4.2.0"
   on_arm do
     url "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.5.7/kustomize_v4.5.7_darwin_arm64.tar.gz"
-    sha256 "7ad70475fe5684f7150f7f1825df5f17652ec812fa65129b756000e9a6b49fff"
+    sha256 "3c1e8b95cef4ff6e52d5f4b8c65b8d9d06b75f42d1cb40986c1d67729d82411a"
   end
   on_intel do
     url "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.5.7/kustomize_v4.5.7_darwin_amd64.tar.gz"
-    sha256 "808d86fc15cec9226dd8b6440f39cfa8e8e31452efc70fb2f35c59529ddebfbf"
+    sha256 "6fd57e78ed0c06b5bdd82750c5dc6d0f992a7b926d114fe94be46d7a7e32b63a"
   end
 
   def install
